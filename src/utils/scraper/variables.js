c6ad4381874aefa8314ed11db4c363bbd77c4d19
MarioLemus <theliarnorland@gmail.com>
2023-01-25 23:09:21 -0600

    first commit

--- src/utils/scraper/variables.js
+++ src/utils/scraper/variables.js
@@ -1,6 +1,9 @@
 import puppeter from 'puppeteer'
 
-const browser = await puppeter.launch({ headless:true })
+const browser = await puppeter.launch({ 
+  headless:true,
+  executablePath: '/usr/bin/chromium-browser'
+})
 
 export {
   browser
